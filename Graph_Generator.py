0ffc492574114dd49e92f852763784db5c72b01b
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-08-01 19:48:47 +0530

    Update Graph_Generator.py

--- Graph_Generator.py
+++ Graph_Generator.py
@@ -2,43 +2,58 @@ import streamlit as st
 import requests
 import json
 
+# Function to call the ChatGPT API
+def call_chat_gpt_api(text):
+    # Replace 'YOUR_API_KEY' with your actual API key
+    api_key = 'sk-...OHEW'
+    url = f'https://api.openai.com/v1/engines/davinci-codex/completions'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+    data = {
+        'prompt': text,
+        'max_tokens': 100,
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()['choices'][0]['text']
+
 def generate_quickchart_url(data):
     # Construct the API URL for quickchart.io
     url = "https://quickchart.io/chart?c=" + data
     return url
 
 def main():
-    st.title("Graph Generator using quickchart.io API")
+    st.title("Graph Generator using ChatGPT and quickchart.io API")
     
     # Get user statements as input
     user_statements = st.text_area("Enter your statements here:")
     
-    # Get the graph type from the user
-    graph_type = st.selectbox("Select Graph Type", ["bar", "line", "area"])
+    # Call the ChatGPT API to make the statement more understandable
+    processed_statements = call_chat_gpt_api(user_statements)
     
-    # Add a Submit button
-    if st.button("Generate Graph"):
-        # Process user statements and create data for the graph
-        # (Replace this with your data processing logic)
-        data_for_graph = {
-            "type": graph_type,
-            "data": {
-                "labels": user_statements.splitlines(),
-                "datasets": [{
-                    "label": "Data",
-                    "data": [10, 20, 15]  # Replace this with your data based on user statements
-                }]
-            }
+    # Process statements and create data for the graph
+    # (Replace this with your data processing logic based on processed_statements)
+    data_for_graph = {
+        "type": "bar",
+        "data": {
+            "labels": ["Label 1", "Label 2", "Label 3"],
+            "datasets": [{
+                "label": "Data",
+                "data": [10, 20, 15]
+            }]
         }
-        
-        # Convert data to JSON string
-        data_json = json.dumps(data_for_graph)
+    }
+    
+    # Convert data to JSON string
+    data_json = json.dumps(data_for_graph)
 
-        # Generate the quickchart.io URL for the graph
-        graph_url = generate_quickchart_url(data_json)
+    # Generate the quickchart.io URL for the graph
+    graph_url = generate_quickchart_url(data_json)
 
-        # Display the graph
-        st.image(graph_url, use_column_width=True)
+    # Display the graph
+    st.image(graph_url, use_column_width=True)
 
 if __name__ == "__main__":
     main()
+    
